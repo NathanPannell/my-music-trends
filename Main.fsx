3d63c0974d2f6ea9f170b581041446337f3f2d95
Nathan Pannell <n8tewolf@gmail.com>
2025-08-20 21:03:45 -0700

    feat: fetch playlist track IDs

--- Main.fsx
+++ Main.fsx
@@ -3,11 +3,20 @@
 #r "nuget: FSharp.Data"
 
 open Microsoft.Data.SqlClient
+open System.Text.RegularExpressions
 open dotenv.net
 open FSharp.Data
 
 
 type AccessToken = JsonProvider<"sample/access_token.json">
+type Playlist = JsonProvider<"sample/playlist.json">
+type User = JsonProvider<"sample/user.json">
+
+let baseApiUrl = "https://api.spotify.com/v1"
+let baseWebUrl = "https://open.spotify.com"
+
+let metaTagPattern = @"<meta name=""music:song"" content=""https://open\.spotify\.com/track/([a-zA-Z0-9]+)""\s*/>"
+let metaTagRegex = new Regex(metaTagPattern)
 
 let envVar name =
     match System.Environment.GetEnvironmentVariable name with
@@ -31,15 +40,63 @@ let getAccessToken () =
                 ]
             )
 
-        return response
-        |> AccessToken.Parse
-        |> _.AccessToken
+        let token = AccessToken.Parse response
+        return token.AccessToken
+    }
+
+// TODO: Make this paginate across the response
+let getPlaylistDirect accessToken playlistId =
+    async {
+        let! response =
+            Http.AsyncRequest (
+                $"{baseApiUrl}/playlists/{playlistId}",
+                httpMethod = HttpMethod.Get,
+                headers = ["Authorization", $"Bearer {accessToken}"]
+            )
+
+        match response.StatusCode, response.Body with
+        | 200, Text json -> 
+            let playlist = Playlist.Parse json
+            return
+                playlist.Tracks.Items
+                |> Array.map _.Track.Id
+                |> List.ofArray
+        | _ ->
+            failwith $"Unable to retrieve playlist {playlistId} via the Web API"
+            return List.empty<string>
+    }
+
+let getPlaylistBrowser playlistId = 
+    async {
+        let! response =
+            Http.AsyncRequest (
+                $"{baseWebUrl}/playlist/{playlistId}",
+                httpMethod = HttpMethod.Get
+            )
+
+        match response.StatusCode, response.Body with
+        | 200, Text json -> 
+            return
+                metaTagRegex.Matches json
+                |> Seq.map (fun m -> m.Groups.[1].Value)
+                |> Seq.toList
+        | _ -> 
+            failwith $"Unable to retrieve playlist {playlistId} via open.spotify.com"
+            return List.empty<string>
     }
 
 let main = 
     async {
         let! accessToken = getAccessToken ()
-        printfn "%s" accessToken
+
+        let djspinthat = "2OecBYmFJPaS5TYvWwd4Lr"
+        let onrepeat = "37i9dQZF1EpzfSVGAiUQT4"
+
+        let! trackIds = getPlaylistDirect accessToken djspinthat
+        printfn "First page of track IDs of a manually created playlist: %A" trackIds
+
+        let! trackIds = getPlaylistBrowser onrepeat
+        printfn "First 30 track IDs for an auto-generated playlist: %A" trackIds
     } 
 
 Async.RunSynchronously main
