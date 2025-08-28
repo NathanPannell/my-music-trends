9214ba6f63e8e08230cc9f1486a85066039053a6
Nathan Pannell <n8tewolf@gmail.com>
2025-08-27 21:45:41 -0700

    feat: dump scraped tracks into staging table

--- Main.fsx
+++ Main.fsx
@@ -6,19 +6,23 @@ open Microsoft.Data.SqlClient
 open System.Text.RegularExpressions
 open dotenv.net
 open FSharp.Data
+open System
+open System.Data
 
+type AccessTokenReponse = JsonProvider<"sample/access_token.json">
+type PlaylistResponse = JsonProvider<"sample/playlist.json">
 
-type AccessToken = JsonProvider<"sample/access_token.json">
-type Playlist = JsonProvider<"sample/playlist.json">
-type User = JsonProvider<"sample/user.json">
+type PlaylistTracksStagingDb = { PlaylistSpotifyId : string ; TrackSpotifyId : string ; Rank : int}
+type PlaylistDb = { PlaylistId : int ; PlaylistSpotifyId : string ; PlaylistOwnerSpotifyId : string ; PlaylistName : string option ; PlaylistDescription : string option ; IsOrdered : bool ; IsSpotifyGenerated : bool}
 
 let baseApiUrl = "https://api.spotify.com/v1"
 let baseWebUrl = "https://open.spotify.com"
 
-let metaTagPattern = @"<meta name=""music:song"" content=""https://open\.spotify\.com/track/([a-zA-Z0-9]+)""\s*/>"
-let metaTagRegex = new Regex(metaTagPattern)
+let metaTagRegex = 
+    @"<meta name=""music:song"" content=""https://open\.spotify\.com/track/([a-zA-Z0-9]+)""\s*/>" 
+    |> Regex
 
-let envVar name =
+let getEnv name =
     match System.Environment.GetEnvironmentVariable name with
     | null | "" ->
         DotEnv.Load()
@@ -28,75 +32,188 @@ let envVar name =
     | value -> value
 
 let getAccessToken () = 
-    async {
-        let! response = 
-            Http.AsyncRequestString(
+    let response = 
+            Http.Request(
                 "https://accounts.spotify.com/api/token",
                 httpMethod = HttpMethod.Post,
                 body = FormValues [
                     "grant_type", "client_credentials"
-                    "client_id", envVar "client_id"
-                    "client_secret", envVar "client_secret"
+                    "client_id", getEnv "client_id"
+                    "client_secret", getEnv "client_secret"
                 ]
             )
 
-        let token = AccessToken.Parse response
-        return token.AccessToken
-    }
+    match response.StatusCode, response.Body with
+    | 200, Text json -> 
+        json
+        |> AccessTokenReponse.Parse
+        |> _.AccessToken
+        |> Some
+    | _ ->
+            None
 
 // TODO: Make this paginate across the response
-let getPlaylistDirect accessToken playlistId =
-    async {
-        let! response =
-            Http.AsyncRequest (
+let rec getPlaylistDirect accessToken  playlistId offset backoff   =
+    let response =
+            Http.Request (
                 $"{baseApiUrl}/playlists/{playlistId}",
                 httpMethod = HttpMethod.Get,
-                headers = ["Authorization", $"Bearer {accessToken}"]
+                headers = ["Authorization", $"Bearer {accessToken |> Option.get}"]
             )
 
-        match response.StatusCode, response.Body with
-        | 200, Text json -> 
-            let playlist = Playlist.Parse json
-            return
-                playlist.Tracks.Items
-                |> Array.map _.Track.Id
-                |> List.ofArray
-        | _ ->
-            failwith $"Unable to retrieve playlist {playlistId} via the Web API"
-            return List.empty<string>
-    }
+    match response.StatusCode, response.Body with
+    | 200, Text json -> 
+        json
+        |> PlaylistResponse.Parse
+        |> _.Tracks.Items
+        |> Array.map _.Track.Id
+        |> List.ofArray
+        |> List.mapi (fun i trackId -> 
+        {
+            PlaylistSpotifyId = playlistId
+            TrackSpotifyId = trackId
+            Rank = i + 1 + offset
+        })
+        |> Some
+    | 429, _ ->
+        if backoff < 2 then getPlaylistDirect accessToken playlistId offset (backoff * 2) else None
+    | _ -> None
 
-let getPlaylistBrowser playlistId = 
-    async {
-        let! response =
-            Http.AsyncRequest (
-                $"{baseWebUrl}/playlist/{playlistId}",
-                httpMethod = HttpMethod.Get
-            )
 
-        match response.StatusCode, response.Body with
-        | 200, Text json -> 
-            return
-                metaTagRegex.Matches json
-                |> Seq.map (fun m -> m.Groups.[1].Value)
-                |> Seq.toList
-        | _ -> 
-            failwith $"Unable to retrieve playlist {playlistId} via open.spotify.com"
-            return List.empty<string>
-    }
+let rec getPlaylistBrowser playlistId backoff = 
+    let response =
+        Http.Request (
+            $"{baseWebUrl}/playlist/{playlistId}",
+            httpMethod = HttpMethod.Get
+        )
 
-let main = 
-    async {
-        let! accessToken = getAccessToken ()
+    match response.StatusCode, response.Body with
+    | 200, Text json -> 
+        json
+        |> metaTagRegex.Matches
+        |> Seq.map _.Groups.[1].Value
+        |> Seq.toList
+        |> List.mapi (fun i trackId -> 
+        {
+            PlaylistSpotifyId = playlistId
+            TrackSpotifyId = trackId
+            Rank = i + 1
+            })
+        |> Some
+    | 429, _ ->
+        if backoff < 2 then getPlaylistBrowser playlistId (backoff * 2) else None
+    | _ -> None
+    
 
-        let djspinthat = "2OecBYmFJPaS5TYvWwd4Lr"
-        let onrepeat = "37i9dQZF1EpzfSVGAiUQT4"
+let getPlaylistTracks accessToken (playlists : list<PlaylistDb>) =
+    playlists
+    |> List.map (fun p ->
+        if p.IsSpotifyGenerated 
+        then getPlaylistBrowser p.PlaylistSpotifyId 1 
+        else getPlaylistDirect accessToken p.PlaylistSpotifyId 0 1
+    )
 
-        let! trackIds = getPlaylistDirect accessToken djspinthat
-        printfn "First page of track IDs of a manually created playlist: %A" trackIds
+type SqlDataReader with
+    member this.TryGet<'T>(ordinal: int) =
+        if this.IsDBNull ordinal then None
+        else Some (this.GetFieldValue<'T> ordinal)
 
-        let! trackIds = getPlaylistBrowser onrepeat
-        printfn "First 30 track IDs for an auto-generated playlist: %A" trackIds
-    } 
+    member this.GetOrFail<'T>(ordinal: int) =
+        this.GetFieldValue<'T> ordinal
 
-Async.RunSynchronously main
+let getPlaylists (connectionString: string) =
+    use conn = new SqlConnection(connectionString)
+    conn.Open()
+    
+    use cmd = new SqlCommand("
+        SELECT 
+            playlist_id,
+            playlist_spotify_id,
+            playlist_owner_spotify_id,
+            playlist_name,
+            playlist_description,
+            is_ordered,
+            is_spotify_generated
+        FROM playlists", conn)
+    
+    use reader = cmd.ExecuteReader()
+
+    let ordPlaylistId = reader.GetOrdinal "playlist_id"
+    let ordPlaylistSpotifyId = reader.GetOrdinal "playlist_spotify_id"
+    let ordPlaylistOwnerSpotifyId = reader.GetOrdinal "playlist_owner_spotify_id"
+    let ordPlaylistName = reader.GetOrdinal "playlist_name"
+    let ordPlaylistDescription = reader.GetOrdinal "playlist_description"
+    let ordIsOrdered = reader.GetOrdinal "is_ordered"
+    let ordIsSpotifyGenerated = reader.GetOrdinal "is_spotify_generated"
+
+    [ while reader.Read() do
+        yield {
+            PlaylistId = reader.GetOrFail<int> ordPlaylistId
+            PlaylistSpotifyId = reader.GetOrFail<string> ordPlaylistSpotifyId
+            PlaylistOwnerSpotifyId = reader.GetOrFail<string> ordPlaylistOwnerSpotifyId
+            PlaylistName = reader.TryGet<string> ordPlaylistName
+            PlaylistDescription = reader.TryGet<string> ordPlaylistDescription
+            IsOrdered = reader.GetOrFail<bool> ordIsOrdered
+            IsSpotifyGenerated = reader.GetOrFail<bool> ordIsSpotifyGenerated
+        }
+    ]
+
+let flattenPlaylistTracks (playlistTracks : list<option<list<PlaylistTracksStagingDb>>>) = 
+    playlistTracks
+    |> List.collect (fun optList ->
+        match optList with
+        | Some items -> items
+        | None -> [])
+
+let toDataTable (rows: seq<PlaylistTracksStagingDb>) =
+    let table = new DataTable()
+    table.Columns.Add("PlaylistSpotifyId", typeof<string>) |> ignore
+    table.Columns.Add("TrackSpotifyId", typeof<string>)   |> ignore
+    table.Columns.Add("Rank", typeof<int>)                |> ignore
+
+    for row in rows do
+        table.Rows.Add(row.PlaylistSpotifyId :> obj, row.TrackSpotifyId :> obj, row.Rank :> obj) |> ignore
+
+    table
+
+let insertRecordsInStaging (connectionString: string) (recordsForStaging: seq<PlaylistTracksStagingDb>) =
+    use conn = new SqlConnection(connectionString)
+    conn.Open()
+    use bulk = new SqlBulkCopy(conn)
+    bulk.DestinationTableName <- "playlist_tracks_staging"
+    bulk.WriteToServer(toDataTable recordsForStaging)
+
+
+let main () = 
+    // Database
+    // 1. Establish db connection
+
+    let connectionString = getEnv "connection_string"
+    printfn "connection string"
+
+    // 2. Fetch all playlist IDs
+    let playlists = getPlaylists connectionString
+    printfn "got playlist ids"
+
+
+    // Spotify
+    // 3. Get access token
+    let accessToken = getAccessToken ()
+    printfn "got access token"
+    
+
+    // 4. Get each playlist's track list
+    let playlistTracks = getPlaylistTracks accessToken playlists
+    printfn "got playlist tracks"
+
+    let recordsForStaging = flattenPlaylistTracks playlistTracks
+    printfn "got records for staging"
+
+
+    // Database 
+    // 5. Dump all records in the staging table
+    do insertRecordsInStaging connectionString recordsForStaging
+    printfn "done"
+
+
+main ()
